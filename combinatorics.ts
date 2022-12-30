af1759191f99b04b6193993c4eb6313b040ed38d
peron <peron_ap@icloud.com>
2022-12-30 21:03:42 +0800

    add cli

--- combinatorics.ts
+++ combinatorics.ts
@@ -1,5 +1,5 @@
 function factorial(n: number): number {
-    return n === 0 ? 1 : n * factorial(n - 1);
+    return n > 0 ? n * factorial(n - 1) : 1;
 }
 
 const Permutations = {
@@ -19,3 +19,31 @@ const Combinations = {
         return Permutations.withoutRepetitions(n, r) / factorial(r);
     },
 };
+
+console.log(`Welcome!
+Type a command to get started.
+Basic structure: <mode> <n> <r>
+
+Modes:
+    pwr - Permutations with repetitions
+    pnr - Permutations without repetitions
+    cwr - Combinations with repetitions
+    cnr - Combinations without repetitions
+`);
+// Command line interface
+while (true) {
+    const [mode, n, r] = prompt('> ')!.split(/\s+/);
+    if (mode === 'q') {
+        break;
+    } else if (mode === 'pwr') {
+        console.log(Permutations.withRepetitions(parseInt(n), parseInt(r)));
+    } else if (mode === 'pnr') {
+        console.log(Permutations.withoutRepetitions(parseInt(n), parseInt(r)));
+    } else if (mode === 'cwr') {
+        console.log(Combinations.withRepetitions(parseInt(n), parseInt(r)));
+    } else if (mode === 'cnr') {
+        console.log(Combinations.withoutRepetitions(parseInt(n), parseInt(r)));
+    } else {
+        console.log('Invalid command');
+    }
+}
