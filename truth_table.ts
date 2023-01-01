d278913447a4f7676d5c4816a95f7b6e7a62d164
peron <peron_ap@icloud.com>
2023-01-01 12:58:35 +0800

    cleanup deps

--- truth_table.ts
+++ truth_table.ts
@@ -1,4 +1,4 @@
-import { Table, Input } from 'https://deno.land/x/cliffy@v0.25.6/mod.ts';
+import { Table } from 'https://deno.land/x/cliffy@v0.25.6/mod.ts';
 
 function generateTruthTable(expressions: string[]): string[][] {
     const result: string[][] = [];
